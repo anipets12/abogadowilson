69a306cc93561340e0b9abfd938a62bb0378bbd9
anipets12 <anipets12@gmail.com>
2025-03-24 17:20:49 -0700

    7

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,4 +1,4 @@
-@const prisma = require('../utils/prisma');
+const prisma = require('../utils/prisma');
 const { hashPassword, comparePassword, generateToken, verifyToken } = require('../utils/auth');
 
 // Cabeceras CORS comunes
